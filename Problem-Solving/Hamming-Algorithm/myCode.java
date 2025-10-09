0c95d5947d79a3834650ff6781a438aae1b7cbd7
AbdullahAsif24 <abdullahasif4045@gmail.com>
2025-10-09 23:16:09 +0500

    .

--- Problem-Solving/Hamming-Algorithm/myCode.java
+++ Problem-Solving/Hamming-Algorithm/myCode.java
@@ -1,10 +1,12 @@
+import java.util.ArrayList;
+
 public class myCode {
 
     public static void main(String[] args) {
         //                  1  2     3           4                       5
         int[] msgBit = { 2, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1 };
 
-        
+        ArrayList hammingBits = new ArrayList<Integer>();
         int z = 0;
         for (int i = 1; i < msgBit.length; i = (int)(Math.pow(2, z))) {
 
@@ -27,9 +29,33 @@ public class myCode {
                 }
                 z++;
 
-                System.out.println("h" + i + " = " + sum + "  " + (sum % 2));
+                hammingBits.add(getHammingBit("odd", sum));
 
             }
         }
+
+        for (int i = 0; i < hammingBits.size(); i++) {
+            System.out.println("h" + (i+1) + " = " + hammingBits.get(i));
+        }
     }
+
+    public static int getHammingBit(String parityScheme, int sum) {
+        int hammingBit;
+        if (parityScheme.equalsIgnoreCase("ODD")) {
+            int r = sum % 2;
+            if (r != 0)
+                hammingBit = 0;
+            else
+                hammingBit = 1; 
+        } else {
+            int r = sum % 2;
+            if (r != 0)
+                hammingBit = 1;
+            else
+                hammingBit = 0;
+        }
+        return hammingBit;
+    }
+
+
 }
