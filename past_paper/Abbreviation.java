5280b615b31cf03838a12e50371c3e0aa5e917d1
AbdullahAsif24 <abdullahasif4045@gmail.com>
2025-11-30 23:04:19 +0500

    .

--- past_paper/Abbreviation.java
+++ past_paper/Abbreviation.java
@@ -1,3 +1,13 @@
+/*
+Write a Java program to read a file line-by-line:
+1. For the first line of the file (e.g., "University of X.Y.Z."), print its abbreviation
+("U.O.X.Y.Z.").
+2. For the second line (e.g., "in my humble opinion"), print its abbreviation ("I.M.H.O.")
+followed by a TAB character.
+*/
+
 public class Abbreviation {
-    
+    public static void main(String[] args) {
+        
+    }
 }
